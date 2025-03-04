507911051d5eb6b9fc3c962c8518d07047f9198a
VedantSinghThakur21 <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-04 22:11:02 +0530

    Defined Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Name: Vedant Singh Thakur
+// PRN: 23070126144
+// Batch: B3
+
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-    }
-}
+                Scanner sc = new Scanner(System.in);
+                StudentManager manager = new StudentManager();
+                int choice;
+
+                do {
+                    System.out.println("\n===== Student Management System =====");
+                    System.out.println("1. Add Student");
+                    System.out.println("2. Display All Students");
+                    System.out.println("3. Search by PRN");
+                    System.out.println("4. Search by Name");
+                    System.out.println("5. Search by Position");
+                    System.out.println("6. Update Student");
+                    System.out.println("7. Delete Student");
+                    System.out.println("0️. Exit");
+                    System.out.print("Enter your choice: ");
+
+                    choice = sc.nextInt();
+
+                    switch (choice) {
+                        case 1:
+                            manager.addStudent(sc);
+                            break;
+                        case 2:
+                            manager.displayStudents();
+                            break;
+                        case 3:
+                            manager.searchByPRN(sc);
+                            break;
+                        case 4:
+                            manager.searchByName(sc);
+                            break;
+                        case 5:
+                            manager.searchByPosition(sc);
+                            break;
+                        case 6:
+                            manager.updateStudent(sc);
+                            break;
+                        case 7:
+                            manager.deleteStudent(sc);
+                            break;
+                        case 0:
+                            System.out.println(" Exiting...");
+                            break;
+                        default:
+                            System.out.println("Invalid choice! Try again.");
+                    }
+                } while (choice != 0);
+
+                sc.close();
+            }
+        }
