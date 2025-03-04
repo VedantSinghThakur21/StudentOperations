e7cc183c21ad42fa1ab76100decf1a384f373d8f
VedantSinghThakur21 <vedant.thakur.btech2023@sitpune.edu.in>
2025-03-04 21:46:34 +0530

    Defined addStudent Function in StudentManader.java

--- src/StudentManager.java
+++ src/StudentManager.java
@@ -7,4 +7,19 @@ public class StudentManager {
         studentList = new ArrayList<>();
     }
 
+    public void addStudent(Scanner sc) {
+        System.out.print("Enter PRN: ");
+        String prn = sc.nextLine();
+        System.out.print("Enter Name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter Date of Birth(DD/MM/YYYY): ");
+        String dob = sc.nextLine();
+        System.out.print("Enter Marks: ");
+        double marks = sc.nextDouble();
+
+        studentList.add(new Student(prn, name, dob, marks));
+        System.out.print("Student Added Successfully!\n");
+
+    }
+
 }
